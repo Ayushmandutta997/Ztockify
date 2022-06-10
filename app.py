bf7e66f773c6595e31fa65ff271d04e1e85d187b
Ayushman Dutta <Ayushman746@gmail.com>
2022-06-11 01:42:24 +0530

    'update'

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ while(i<50):
         i=i+1
     else:
         x_input=x_input.reshape(1,-1)
-        x_input=x_input.reshape((1,n_steps,1))
+        # x_input=x_input.reshape((1,n_steps,1))
         yhat=model.predict(x_input,verbose=0)
         print(yhat[0])
         temp_list.extend(yhat[0].tolist())
